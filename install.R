dda4fede965b162d751a53807b9546125236c503
Chris Kennedy <chrisken@gmail.com>
2018-02-28 15:37:51 -0800

    update binder packages for randomforest

--- install.R
+++ install.R
@@ -1,4 +1,7 @@
 install.packages("devtools")
 devtools::install_github(c("ecpolley/SuperLearner", "ck37/ck37r"))
-ck37r::load_packages(c("rpart", "rpart.plot", "partykit", "mlr", "caret",
-                       "h2o"), auto_install = TRUE)
+cran_packages =
+  c("rpart", "rpart.plot", "partykit", "mlr", "caret", "car", "caret",
+    "ggplot2", "lattice", "plotmo", "randomForest", "ROCR",
+    "survival", "xgboost", "h2o")
+ck37r::load_packages(cran_packages, auto_install = TRUE)
