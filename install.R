ed5254b50779e8c581e67cacbac07c32ed1cae88
Chris Kennedy <chrisken@gmail.com>
2018-03-13 20:44:00 -0700

    install.R: remove duplicated "caret" in listing

--- install.R
+++ install.R
@@ -1,7 +1,7 @@
 install.packages("devtools")
 devtools::install_github(c("ecpolley/SuperLearner", "ck37/ck37r"))
 cran_packages =
-  c("rpart", "rpart.plot", "partykit", "mlr", "caret", "car", "caret",
+  c("rpart", "rpart.plot", "partykit", "mlr", "car", "caret",
     "ggplot2", "lattice", "plotmo", "randomForest", "ROCR",
     "survival", "xgboost", "h2o")
 ck37r::load_packages(cran_packages, auto_install = TRUE)
